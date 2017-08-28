6df61e68e504d59fa5573941f95de5e37f485e94
Jeremiah Diboll <jdiboll@gmail.com>
2017-08-28 10:53:25 -0400

    ef framework added. now to model data.

---
+++ todolistContext.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+
+namespace ToDoListApp
+{
+    public partial class todolistContext : DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+#warning To protect potentially sensitive information in your connection string, you should move it out of source code. See http://go.microsoft.com/fwlink/?LinkId=723263 for guidance on storing connection strings.
+                optionsBuilder.UseNpgsql(@"Host=localhost;Database=todolist;Username=dev;Password=brit1336");
+            }
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {}
+    }
+}
